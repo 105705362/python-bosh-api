3dafd73ee281607446cdee6efe0d995edbdc0ccd
Chaos Eternal <chaos@shlug.org>
2017-12-07 23:22:58 +0800

    bugfix

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='bosh_api',
       version='0.0.1',
-      py_modules=['vcap_config'],
+      py_modules=['bosh_api'],
       requires=['requests']
       )
